5620210df67a20451e31fc48a3f28951fcd48dc9
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2022-11-27 11:27:36 -0800

    dk rune assets
    part of an uprezzed dk rune project

--- KBT_UIAssets.lua
+++ KBT_UIAssets.lua
@@ -15,441 +15,474 @@ LOGO = {
 };
 
 --Uprezzes/reworks/ports of game icons
-ICON = {
+ICONS = {
 	--death knight
 	deathknight_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\deathknight_flat",
 		width = 512,
 		height = 512,
 	},
 	deathknight_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\deathknight_flatborder",
 		width = 512,
 		height = 512,
 	},
 	deathknight_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\deathknight_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	deathknight_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\deathknight_round",
 		width = 512,
 		height = 512,
 	},
 	deathknight_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\deathknight_square",
 		width = 512,
 		height = 512,
 	},
 	deathknight_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\deathknight_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--demon hunter
 	demonhunter_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\demonhunter_flat",
 		width = 512,
 		height = 512,
 	},
 	demonhunter_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\demonhunter_flatborder",
 		width = 512,
 		height = 512,
 	},
 	demonhunter_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\demonhunter_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	demonhunter_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\demonhunter_round",
 		width = 512,
 		height = 512,
 	},
 	demonhunter_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\demonhunter_square",
 		width = 512,
 		height = 512,
 	},
 	demonhunter_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\demonhunter_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--druid
 	druid_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\druid_flat",
 		width = 512,
 		height = 512,
 	},
 	druid_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\druid_flatborder",
 		width = 512,
 		height = 512,
 	},
 	druid_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\druid_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	druid_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\druid_round",
 		width = 512,
 		height = 512,
 	},
 	druid_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\druid_square",
 		width = 512,
 		height = 512,
 	},
 	druid_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\druid_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--evoker
 	evoker_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\evoker_flat",
 		width = 512,
 		height = 512,
 	},
 	evoker_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\evoker_flatborder",
 		width = 512,
 		height = 512,
 	},
 	evoker_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\evoker_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	evoker_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\evoker_round",
 		width = 512,
 		height = 512,
 	},
 	evoker_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\evoker_square",
 		width = 512,
 		height = 512,
 	},
 	evoker_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\evoker_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--evoker 2
 	evoker2_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker2_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\evoker2_flat",
 		width = 512,
 		height = 512,
 	},
 	evoker2_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker2_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\evoker2_flatborder",
 		width = 512,
 		height = 512,
 	},
 	evoker2_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker2_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\evoker2_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	evoker2_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker2_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\evoker2_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--hunter
 	hunter_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\hunter_flat",
 		width = 512,
 		height = 512,
 	},
 	hunter_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\hunter_flatborder",
 		width = 512,
 		height = 512,
 	},
 	hunter_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\hunter_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	hunter_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\hunter_round",
 		width = 512,
 		height = 512,
 	},
 	hunter_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\hunter_square",
 		width = 512,
 		height = 512,
 	},
 	hunter_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\hunter_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--mage
 	mage_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\mage_flat",
 		width = 512,
 		height = 512,
 	},
 	mage_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\mage_flatborder",
 		width = 512,
 		height = 512,
 	},
 	mage_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\mage_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	mage_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\mage_round",
 		width = 512,
 		height = 512,
 	},
 	mage_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\mage_square",
 		width = 512,
 		height = 512,
 	},
 	mage_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\mage_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--monk
 	monk_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\monk_flat",
 		width = 512,
 		height = 512,
 	},
 	monk_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\monk_flatborder",
 		width = 512,
 		height = 512,
 	},
 	monk_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\monk_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	monk_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\monk_round",
 		width = 512,
 		height = 512,
 	},
 	monk_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\monk_square",
 		width = 512,
 		height = 512,
 	},
 	monk_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\monk_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--paladin
 	paladin_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\paladin_flat",
 		width = 512,
 		height = 512,
 	},
 	paladin_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\paladin_flatborder",
 		width = 512,
 		height = 512,
 	},
 	paladin_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\paladin_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	paladin_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\paladin_round",
 		width = 512,
 		height = 512,
 	},
 	paladin_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\paladin_square",
 		width = 512,
 		height = 512,
 	},
 	paladin_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\paladin_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--priest
 	priest_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\priest_flat",
 		width = 512,
 		height = 512,
 	},
 	priest_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\priest_flatborder",
 		width = 512,
 		height = 512,
 	},
 	priest_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\priest_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	priest_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\priest_round",
 		width = 512,
 		height = 512,
 	},
 	priest_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\priest_square",
 		width = 512,
 		height = 512,
 	},
 	priest_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\priest_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--rogue
 	rogue_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\rogue_flat",
 		width = 512,
 		height = 512,
 	},
 	rogue_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\rogue_flatborder",
 		width = 512,
 		height = 512,
 	},
 	rogue_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\rogue_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	rogue_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\rogue_round",
 		width = 512,
 		height = 512,
 	},
 	rogue_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\rogue_square",
 		width = 512,
 		height = 512,
 	},
 	rogue_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\rogue_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--shaman
 	shaman_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\shaman_flat",
 		width = 512,
 		height = 512,
 	},
 	shaman_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\shaman_flatborder",
 		width = 512,
 		height = 512,
 	},
 	shaman_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\shaman_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	shaman_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\shaman_round",
 		width = 512,
 		height = 512,
 	},
 	shaman_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\shaman_square",
 		width = 512,
 		height = 512,
 	},
 	shaman_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\shaman_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--warlock
 	warlock_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warlock_flat",
 		width = 512,
 		height = 512,
 	},
 	warlock_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warlock_flatborder",
 		width = 512,
 		height = 512,
 	},
 	warlock_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warlock_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	warlock_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warlock_round",
 		width = 512,
 		height = 512,
 	},
 	warlock_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warlock_square",
 		width = 512,
 		height = 512,
 	},
 	warlock_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warlock_warcraftflat",
 		width = 512,
 		height = 512,
 	},
 
 	--warrior
 	warrior_flat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_flat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warrior_flat",
 		width = 512,
 		height = 512,
 	},
 	warrior_flatborder = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_flatborder",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warrior_flatborder",
 		width = 512,
 		height = 512,
 	},
 	warrior_flatborder2 = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_flatborder2",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warrior_flatborder2",
 		width = 512,
 		height = 512,
 	},
 	warrior_round = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_round",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warrior_round",
 		width = 512,
 		height = 512,
 	},
 	warrior_square = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_square",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warrior_square",
 		width = 512,
 		height = 512,
 	},
 	warrior_warcraftflat = {
-		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_warcraftflat",
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\warrior_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+--------------------------------------------------------------------------------------------------------
+
+	--dk runes (desaturated, will require SetVertexColor)
+	dkrune_bl_1 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\dkrune_bl_1",
+		width = 512,
+		height = 512,
+	},
+	dkrune_bl_2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\dkrune_bl_2",
+		width = 512,
+		height = 512,
+	},
+	dkrune_fr_1 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\dkrune_fr_1",
+		width = 512,
+		height = 512,
+	},
+	dkrune_fr_2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\dkrune_fr_2",
+		width = 512,
+		height = 512,
+	},
+	dkrune_uh_1 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\dkrune_uh_1",
+		width = 512,
+		height = 512,
+	},
+	dkrune_uh_2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\ICONS\\dkrune_uh_2",
 		width = 512,
 		height = 512,
 	},
