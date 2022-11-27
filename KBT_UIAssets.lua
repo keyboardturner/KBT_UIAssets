872b9e0f35e6d88503c3ac1e5e5e2c6c0fc21c4d
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2022-11-27 11:18:21 -0800

    uprezzed flat class icon textures
    from flat icon texture pack project, refitted to 512x512 for ingame

--- KBT_UIAssets.lua
+++ KBT_UIAssets.lua
@@ -7,7 +7,453 @@ BG = {
 
 --Logos, Crests, Sigils, Symbols, etc.
 LOGO = {
-	Evoker = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\Evoker", --1024x1024
+	Evoker = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\Evoker", --1024x1024
+		width = 1024,
+		height = 1024,
+	},
+};
+
+--Uprezzes/reworks/ports of game icons
+ICON = {
+	--death knight
+	deathknight_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_flat",
+		width = 512,
+		height = 512,
+	},
+	deathknight_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_flatborder",
+		width = 512,
+		height = 512,
+	},
+	deathknight_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	deathknight_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_round",
+		width = 512,
+		height = 512,
+	},
+	deathknight_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_square",
+		width = 512,
+		height = 512,
+	},
+	deathknight_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\deathknight_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--demon hunter
+	demonhunter_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_flat",
+		width = 512,
+		height = 512,
+	},
+	demonhunter_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_flatborder",
+		width = 512,
+		height = 512,
+	},
+	demonhunter_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	demonhunter_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_round",
+		width = 512,
+		height = 512,
+	},
+	demonhunter_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_square",
+		width = 512,
+		height = 512,
+	},
+	demonhunter_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\demonhunter_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--druid
+	druid_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_flat",
+		width = 512,
+		height = 512,
+	},
+	druid_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_flatborder",
+		width = 512,
+		height = 512,
+	},
+	druid_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	druid_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_round",
+		width = 512,
+		height = 512,
+	},
+	druid_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_square",
+		width = 512,
+		height = 512,
+	},
+	druid_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\druid_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--evoker
+	evoker_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_flat",
+		width = 512,
+		height = 512,
+	},
+	evoker_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_flatborder",
+		width = 512,
+		height = 512,
+	},
+	evoker_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	evoker_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_round",
+		width = 512,
+		height = 512,
+	},
+	evoker_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_square",
+		width = 512,
+		height = 512,
+	},
+	evoker_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--evoker 2
+	evoker2_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker2_flat",
+		width = 512,
+		height = 512,
+	},
+	evoker2_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker2_flatborder",
+		width = 512,
+		height = 512,
+	},
+	evoker2_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker2_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	evoker2_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\evoker2_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--hunter
+	hunter_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_flat",
+		width = 512,
+		height = 512,
+	},
+	hunter_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_flatborder",
+		width = 512,
+		height = 512,
+	},
+	hunter_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	hunter_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_round",
+		width = 512,
+		height = 512,
+	},
+	hunter_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_square",
+		width = 512,
+		height = 512,
+	},
+	hunter_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\hunter_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--mage
+	mage_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_flat",
+		width = 512,
+		height = 512,
+	},
+	mage_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_flatborder",
+		width = 512,
+		height = 512,
+	},
+	mage_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	mage_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_round",
+		width = 512,
+		height = 512,
+	},
+	mage_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_square",
+		width = 512,
+		height = 512,
+	},
+	mage_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\mage_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--monk
+	monk_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_flat",
+		width = 512,
+		height = 512,
+	},
+	monk_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_flatborder",
+		width = 512,
+		height = 512,
+	},
+	monk_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	monk_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_round",
+		width = 512,
+		height = 512,
+	},
+	monk_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_square",
+		width = 512,
+		height = 512,
+	},
+	monk_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\monk_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--paladin
+	paladin_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_flat",
+		width = 512,
+		height = 512,
+	},
+	paladin_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_flatborder",
+		width = 512,
+		height = 512,
+	},
+	paladin_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	paladin_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_round",
+		width = 512,
+		height = 512,
+	},
+	paladin_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_square",
+		width = 512,
+		height = 512,
+	},
+	paladin_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\paladin_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--priest
+	priest_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_flat",
+		width = 512,
+		height = 512,
+	},
+	priest_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_flatborder",
+		width = 512,
+		height = 512,
+	},
+	priest_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	priest_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_round",
+		width = 512,
+		height = 512,
+	},
+	priest_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_square",
+		width = 512,
+		height = 512,
+	},
+	priest_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\priest_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--rogue
+	rogue_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_flat",
+		width = 512,
+		height = 512,
+	},
+	rogue_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_flatborder",
+		width = 512,
+		height = 512,
+	},
+	rogue_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	rogue_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_round",
+		width = 512,
+		height = 512,
+	},
+	rogue_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_square",
+		width = 512,
+		height = 512,
+	},
+	rogue_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\rogue_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--shaman
+	shaman_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_flat",
+		width = 512,
+		height = 512,
+	},
+	shaman_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_flatborder",
+		width = 512,
+		height = 512,
+	},
+	shaman_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	shaman_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_round",
+		width = 512,
+		height = 512,
+	},
+	shaman_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_square",
+		width = 512,
+		height = 512,
+	},
+	shaman_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\shaman_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--warlock
+	warlock_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_flat",
+		width = 512,
+		height = 512,
+	},
+	warlock_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_flatborder",
+		width = 512,
+		height = 512,
+	},
+	warlock_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	warlock_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_round",
+		width = 512,
+		height = 512,
+	},
+	warlock_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_square",
+		width = 512,
+		height = 512,
+	},
+	warlock_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warlock_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
+	--warrior
+	warrior_flat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_flat",
+		width = 512,
+		height = 512,
+	},
+	warrior_flatborder = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_flatborder",
+		width = 512,
+		height = 512,
+	},
+	warrior_flatborder2 = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_flatborder2",
+		width = 512,
+		height = 512,
+	},
+	warrior_round = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_round",
+		width = 512,
+		height = 512,
+	},
+	warrior_square = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_square",
+		width = 512,
+		height = 512,
+	},
+	warrior_warcraftflat = {
+		url = "Interface\\AddOns\\KBT_UIAssets\\Assets\\LOGO\\warrior_warcraftflat",
+		width = 512,
+		height = 512,
+	},
+
 };
 
 --Borders that match backdrop edgeFile atlas
@@ -32,14 +478,17 @@ function startFrame:Initialize(event, arg1)
 		startFrame:UnregisterEvent("ADDON_LOADED")
 
 		local testFrame = CreateFrame("Frame", "KBT_UITestFrame", UIParent, "BackdropTemplate")
-		testFrame:SetSize(500,500)
+		testFrame:SetSize(KBT_UI.LOGO.Evoker.width,KBT_UI.LOGO.Evoker.height)
 		testFrame:SetPoint("CENTER")
-		testFrame:SetBackdrop({ bgFile = KBT_UI.LOGO.Evoker, edgeFile = "Interface/Tooltips/UI-Tooltip-Border-Maw", tile = false, edgeSize = 15, tileSize = 10, insets = { left = 3, right = 3, top = 3, bottom = 3, }, })
+		testFrame:SetBackdrop({ bgFile = KBT_UI.LOGO.Evoker.url, edgeFile = "Interface/Tooltips/UI-Tooltip-Border-Maw", tile = false, edgeSize = 15, tileSize = 10, insets = { left = 3, right = 3, top = 3, bottom = 3, }, })
 		testFrame:SetAlpha(1)
 		testFrame:SetBackdropColor(1,1,1,1)
 		testFrame:SetBackdropBorderColor(1,1,1,1)
 
-		
+		for k, v in pairs(KBT_UI.LOGO) do
+			
+			print("k: " .. KBT_UI.LOGO[k]["url"]) -- the name
+		end
 
 	end	
 end
